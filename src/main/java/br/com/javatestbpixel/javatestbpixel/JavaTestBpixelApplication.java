f041e119156e471273194536e56842d8b8ef272d
Weverton Reis <weverton.sant.reis@gmail.com>
2018-06-19 08:49:55 -0300

    Iniciando o projeto de treinamento

---
+++ src/main/java/br/com/javatestbpixel/javatestbpixel/JavaTestBpixelApplication.java
@@ -0,0 +1,12 @@
+package br.com.javatestbpixel.javatestbpixel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestBpixelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestBpixelApplication.class, args);
+	}
+}
